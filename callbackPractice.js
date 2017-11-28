2570f31ed0d92930fad66ad53d78fb0718ad50ef
Hammer_Keith <keith@michaelrobertson.com>
2017-11-28 17:36:05 -0600

    Finished

--- callbackPractice.js
+++ callbackPractice.js
@@ -18,11 +18,10 @@ and WHAT YOU SHOULD WRITE is the sayHi function that makes the code above work:
 
 // 1. Write a function called first that takes in two parameters, an array and a callback function, then invokes the callback function, passing in the first element in the array as it's argument.  (see the sayHi function on line 12 for reference)
 
-var first = (arr, doit) => {
-  doit(arr);
+var first = function(arr, call){
+  call(arr[0]);
 }
 
-  
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 
 first(names, function(firstName){
@@ -52,10 +51,10 @@ last(names, function(lastName){
 
   //Code Here
 
-  var multiply = (num1, num1, call){
+  var multiply = function(num1, num2, call){
     call(num1*num2);
   }
-  
+
 multiply(4, 3, function(answer){
   console.log('The answer is ' + answer); //should console.log 12
 })
@@ -69,8 +68,15 @@ multiply(4, 3, function(answer){
 
   //Code Here 
 
-/*
 
+var contains = function(arr, name, call) {
+  arr.forEach(element => {
+    if (element === name){
+      call(true);
+    }
+    call(false);    
+  });
+}
 
 contains(names, 'Colt', function(result){
   if(result === true){
@@ -80,14 +86,7 @@ contains(names, 'Colt', function(result){
   }
 });
 
-var contains = (arr, name, call) =>{
-  arr.forEach(element => {
-    if (element === name){
-      call(true);
-    }
-    call(false);    
-  });
-}
+
 
 // 5. Write a function called uniq that takes in an array and a callback function.
 // Remove any duplicate values from the array, and invoke the callback with the modified array as an argument.
@@ -100,15 +99,15 @@ var contains = (arr, name, call) =>{
     for(var i = (arr.length - 1);i>=0;i--){
       for(var j = (arr.length - 1);j>=0;j--)
       {
-        if (arr[i]===arr[j]){
+        if (i === j){}
+        else if (arr[i]===arr[j]){
           arr.splice(j, 1);
         }
       }
       }
       call(arr)
     }
-    
-    uniq(str, function(uniqArr){
+    uniq(names, function(uniqArr){
       console.log('The new names array with all the duplicate items removed is ', uniqArr);
     });
   
@@ -117,11 +116,18 @@ var contains = (arr, name, call) =>{
 
     //Code Here 
 
-
-
-each(names, function(item, indice){
-  console.log('The item in the ' + indice + ' position is ' + item)
-});
+    var each = function(arr, call){
+      var q = 0;
+      arr.forEach(element => {
+        call(element, q)
+        q++
+      });
+    }
+    
+  
+    each(names, function(item, indice){
+      console.log('The item in the ' + indice + ' position is ' + item)
+    });
 
 
 
@@ -129,7 +135,13 @@ each(names, function(item, indice){
 // When the correct user object is found, invoke the callback with the user object as an argument.
 
 // Code here
-
+var getUserById = function(arr, str, call){
+  arr.forEach(element => {
+    if(element.id === str){
+      call(element)
+    }
+  });
+}
 
 
 var users = [
@@ -156,4 +168,3 @@ var users = [
 getUserById(users, '16t', function(user){
   console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address); 
 });
-*/
